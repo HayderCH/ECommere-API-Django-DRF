c254405cb3fef31d1bd741db39b4fada4c329b26
HayderCH <hayder.chakroun@esprim.tn>
2025-05-20 22:11:09 +0100

    added router modified view

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,13 +1,9 @@
-from django.shortcuts import render
+
 from rest_framework import viewsets
-from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
 
 
-class CategoryView(viewsets.ViewSet):
-    
-    def list(self, request):
-        queryset = Category.objects.all()
-        serializer = CategorySerializer(queryset, many=True)
-        return Response(serializer.data)
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
