dc9da1a159d6af0f528a452eeeffbf0505b0acc4
HayderCH <hayder.chakroun@esprim.tn>
2025-05-20 22:31:22 +0100

    added swagger ui

--- manage.py
+++ manage.py
@@ -4,12 +4,13 @@ import os
 import sys
 from ecommerce.settings import base
 
+
 def main():
     """Run administrative tasks."""
     if base.DEBUG:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings.local')
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings.local")
     else:
-         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings.production')
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings.production")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -21,5 +22,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
